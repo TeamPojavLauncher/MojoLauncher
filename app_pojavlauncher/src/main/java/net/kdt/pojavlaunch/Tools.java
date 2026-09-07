70f4adb931c64c5072d68ee84c02cde8300fcadf
Mahdin4sure <183899034+Mahdin4sure@users.noreply.github.com>
2026-09-08 02:20:17 +0600

    add some fixes before pojav-legacy release

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/Tools.java
@@ -100,7 +100,7 @@ public final class Tools {
             .setPrettyPrinting()
             .create();
 
-    public static final String URL_HOME = "https://mojolauncher.ru";
+    public static final String URL_HOME = "https://pojav.ru";
     public static String NATIVE_LIB_DIR;
     public static String DIR_DATA; //Initialized later to get context
     public static File DIR_CACHE;
